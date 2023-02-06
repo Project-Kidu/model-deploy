3c0012820307fc147b9210c188d4daad9d26b503
gokul-pv <25975535+gokul-pv@users.noreply.github.com>
2023-02-06 06:30:31 +0000

    Added code to deploy lambda using cdk

--- test/test.py
+++ test/test.py
@@ -1,48 +1,78 @@
-import argparse
-import json
-import logging
 import os
-
+import json
 import boto3
-from botocore.exceptions import ClientError
+import logging
+import argparse
+import requests
+from pathlib import Path
+import base64
+import albumentations as A
+from PIL import Image
+import numpy as np
+import io
 
 logger = logging.getLogger(__name__)
-sm_client = boto3.client("sagemaker")
+api_client = boto3.client('apigatewayv2', region_name="ap-south-1")
 
 
-def invoke_endpoint(endpoint_name):
-    """
-    Add custom logic here to invoke the endpoint and validate reponse
-    """
-    return {"endpoint_name": endpoint_name, "success": True}
+def get_api_url(api_name):
+    list_apis = api_client.get_apis()['Items']
+    filtered_apis = [api for api in list_apis if api["Name"] == api_name]
+    api_url = filtered_apis[0]['ApiEndpoint']
+    return api_url
 
 
-def test_endpoint(endpoint_name):
-    """
-    Describe the endpoint and ensure InSerivce, then invoke endpoint.  Raises exception on error.
-    """
-    error_message = None
+def test_api(name, url, image_path):
+    transforms = A.Compose(
+                [
+                    A.GaussNoise(p=0.2),
+                    A.OneOf(
+                        [
+                            A.MotionBlur(p=0.2),
+                            A.MedianBlur(blur_limit=3, p=0.1),
+                            A.Blur(blur_limit=3, p=0.1),
+                        ],
+                        p=0.2,
+                    ),
+                ]
+            ) 
+
     try:
-        # Ensure endpoint is in service
-        response = sm_client.describe_endpoint(EndpointName=endpoint_name)
-        status = response["EndpointStatus"]
-        if status != "InService":
-            error_message = f"SageMaker endpoint: {endpoint_name} status: {status} not InService"
-            logger.error(error_message)
-            raise Exception(error_message)
+        for file in image_path.glob("*/*.jpg"):
+            print(f"{file=}")
 
-        # Output if endpoint has data capture enbaled
-        endpoint_config_name = response["EndpointConfigName"]
-        response = sm_client.describe_endpoint_config(EndpointConfigName=endpoint_config_name)
-        if "DataCaptureConfig" in response and response["DataCaptureConfig"]["EnableCapture"]:
-            logger.info(f"data capture enabled for endpoint config {endpoint_config_name}")
+            image = Image.open(file)
+            image = np.array(image)
+            augmented_image = transforms(image=image)["image"]
 
-        # Call endpoint to handle
-        return invoke_endpoint(endpoint_name)
-    except ClientError as e:
-        error_message = e.response["Error"]["Message"]
-        logger.error(error_message)
-        raise Exception(error_message)
+            buf = io.BytesIO()
+            aug_image = Image.fromarray(augmented_image)
+            aug_image.save(buf, format="JPEG")
+            
+            ext = file.name.split('.')[-1]
+            prefix = f'data:image/{ext};base64,'
+            base64_data = prefix + base64.b64encode(buf.getvalue()).decode('utf-8')
+
+            payload = json.dumps({"body": [base64_data]})
+            headers = {"content-type": "application/json"}
+
+            response = requests.request("POST", url, json=payload, headers=headers)
+            response.raise_for_status()  # if status !=200 raise exception
+
+            prediction = response.json()[0]
+            prediction_sorted = sorted(prediction.items(), key=lambda x:x[1], reverse=True)
+            print(prediction_sorted)
+
+            assert prediction_sorted[0][0] == file.parent.stem
+        
+        return "Success"
+
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+    except AssertionError as err:
+        logger.debug("Assert failed")
+        raise SystemExit(err)
 
 
 if __name__ == "__main__":
@@ -60,11 +90,20 @@ if __name__ == "__main__":
     with open(args.import_build_config, "r") as f:
         config = json.load(f)
 
-    # Get the endpoint name from sagemaker project name
-    endpoint_name = "{}-{}".format(
-        config["Parameters"]["SageMakerProjectName"], config["Parameters"]["StageName"]
-    )
-    results = test_endpoint(endpoint_name)
+    # Get the api name from sagemaker project name
+    api_name = "{}-{}".format(config["Parameters"]["SageMakerProjectName"], config["Parameters"]["StageName"])
+
+    api_url = get_api_url(api_name)
+
+    # send a test data point to the api and verifying the response status code
+    resource_path = Path("test/resources/intel-scene")
+    response = test_api(api_name, api_url, resource_path)
+
+    results = {
+            'api_name': api_name,
+            'api_url': api_url,
+            'response': response
+        }
 
     # Print results and write to file
     logger.debug(json.dumps(results, indent=4))
